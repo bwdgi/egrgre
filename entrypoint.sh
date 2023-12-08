00bdc251e94f00fc0ffb5a73119aafa163c4f2cc
bwdgi <136963669+bwdgi@users.noreply.github.com>
2023-12-08 02:52:04 +0000

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
-SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+UUID=${UUID:-'f0824202-3128-4c1e-ac61-8abe5fe10331'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/f0824202-3128-4c1e-ac61-8abe5fe10331-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/f0824202-3128-4c1e-ac61-8abe5fe10331-vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/f0824202-3128-4c1e-ac61-8abe5fe10331-trojan'}
+SS_WSPATH=${SS_WSPATH:-'/f0824202-3128-4c1e-ac61-8abe5fe10331-shadowsocks'}
 
 generate_config() {
   cat > config.json << EOF
